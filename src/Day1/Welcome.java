e23cf2d01b1c7e8995401b88d6a442b9382abe16
yajib16 <50978166+yajib16@users.noreply.github.com>
2022-01-14 12:40:33 -0600

    First commit

---
+++ src/Day1/Welcome.java
@@ -0,0 +1,12 @@
+package Day1;
+
+public class Welcome {
+
+    int age=5;
+    String name= "Ram Bhattarai";
+    double phone = 9819382048d;
+    public  void main(String[] args){
+        System.out.println("Hello there!\n Information of User\n");
+        System.out.println("UserName:" + name + "UserAge:" + age + "UserPhone:" + phone);
+    }
+}
