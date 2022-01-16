14b9cbbdd741e1e2eb6fe60f1d3de7c27341d4f4
yajib16 <50978166+yajib16@users.noreply.github.com>
2022-01-16 15:47:13 -0600

    Data types

---
+++ src/Day1/DataTypes.java
@@ -0,0 +1,16 @@
+package Day1;
+
+import java.util.logging.Logger;
+
+public class DataTypes {
+    static int age;
+    static String color="green";
+    static double phone;
+    static char gender;
+    static String name;
+    static float height;
+    static  String address;
+    public static void main(String[] args){
+        Logger.getGlobal().info(String.valueOf(height+ "\t"+color));
+    }
+}
