15e812e5adf68222a4262e1f137227dc564093f1
yajib16 <50978166+yajib16@users.noreply.github.com>
2022-01-19 11:43:07 -0600

    IF STATEMENT

---
+++ src/Day2/JavaCondition.java
@@ -0,0 +1,21 @@
+package Day2;
+/*
+ * In this section we mainly check the condition and returns the statement
+ * IF STATEMENT
+ * IF-ELSE STATEMENT
+ * ELSE-IF STATEMENT
+ * NESTED STATEMENTS */
+
+import java.util.logging.Logger;
+
+public class JavaCondition  {              //IF STATEMENT ONLY
+
+    public static  void main(String []args){
+        double maths = 90, science = 85, social = 82, english = 90,health = 89;
+        double sumPercent = ((maths+science+social+english+health)/500)*100;
+        if (sumPercent>+80){
+            Logger.getGlobal().info(String.valueOf("You Percentage : \t" + sumPercent + "%"));
+        }
+
+    }
+}
